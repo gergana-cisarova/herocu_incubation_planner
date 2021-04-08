efb13f45d35f536c0c38e851d2bdd293cb5d8559
gergana-cisarova <gergana.cisarova@gmail.com>
2021-04-09 01:13:28 +0300

    Changed LocalDateTime to LocalDate

--- src/main/java/com/example/incubation_planner/web/ProjectRestController.java
+++ src/main/java/com/example/incubation_planner/web/ProjectRestController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RequestMapping("/manageProjects")
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class ProjectRestController {
 
     private final ProjectService projectService;
